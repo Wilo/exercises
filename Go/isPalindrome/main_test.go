30d47dce1414c0fba4e0afa7f23a918d8f9b3895
William Méndez <wmendezc19@gmail.com>
2021-05-03 06:36:13 -0500

    Fix: fix typo and make ReverseText test for Go

--- Go/isPalindrome/main_test.go
+++ Go/isPalindrome/main_test.go
@@ -12,7 +12,7 @@ var (
 	empty_text         string = ""
 )
 
-func TesIsPalindrome(t *testing.T) {
+func TestIsPalindrome(t *testing.T) {
 	t.Run("Is Palindrome", func(t *testing.T) {
 		t.Run("When the text is palindrome", func(t *testing.T) {
 			result := IsPalindrome(palindrome_name)
@@ -24,7 +24,9 @@ func TesIsPalindrome(t *testing.T) {
 			assert.Equal(t, result, false)
 		})
 	})
+}
 
+func TestReverse(t *testing.T) {
 	t.Run("Reverse Text", func(t *testing.T) {
 		t.Run("When the text is correct", func(t *testing.T) {
 			result := Reverse(palindrome_name)
